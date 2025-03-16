43acb68ecd1739def7b1a07e31952685aebfff73
Martin <martimartineez@gmail.com>
2025-03-16 03:23:05 -0300

    abbber

---
+++ Tercer año/Primer semestre/Objetos2/practicas/ejercicio2/Piedra.java
@@ -0,0 +1,9 @@
+package ar.edu.unlp.info.oo1.ejercicio2;
+
+public class Piedra extends Figura{
+	
+	public Piedra() {
+		super("piedra");
+	}
+	
+}
---
+++ Tercer año/Primer semestre/Objetos2/practicas/ejercicio2/Spock.java
@@ -0,0 +1,10 @@
+package ar.edu.unlp.info.oo1.ejercicio2;
+
+public class Spock extends Figura{
+	
+	public Spock() {
+		super("spock");
+	}
+	
+
+}
