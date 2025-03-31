a2500ce56e15c335f4ce92c2f8d91a29d6d20277
Martin <martimartineez@gmail.com>
2025-03-31 20:12:06 -0300

    update

--- Tercer año/Primer semestre/Objetos2/practicas/ejercicio1/Itweet.java
+++
@@ -1,6 +0,0 @@
-package ar.edu.unlp.info.oo1.ejercicio1;
-
-public interface Itweet {
-	
-	public void eliminar();
-}
--- Tercer año/Primer semestre/Objetos2/practicas/ejercicio2/Piedra.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.unlp.info.oo1.ejercicio2;
-
-public class Piedra extends Figura{
-	
-	public Piedra() {
-		super("piedra");
-	}
-	
-}
